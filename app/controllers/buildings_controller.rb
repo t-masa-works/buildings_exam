8c24db4f81ec0b219c8f7dc0251a66e3ed2ee79f
tobe masa <t.masa.works@gmail.com>
2023-08-11 11:41:02 +0900

    文言修正、更新登録ボタン分岐

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,32 +1,27 @@
 class BuildingsController < ApplicationController
-
-  # GET /buildings or /buildings.json
   def index
     @buildings = Building.all
   end
 
-  # GET /buildings/1 or /buildings/1.json
   def show
     set_building
   end
 
-  # GET /buildings/new
   def new
     @building = Building.new
     2.times{ @building.stations.build }
   end
 
-  # GET /buildings/1/edit
   def edit
     set_building
   end
 
-  # POST /buildings or /buildings.json
   def create
     @building = Building.new(building_params)
+
     respond_to do |format|
       if @building.save
-        format.html { redirect_to @building, notice: "Building was successfully created." }
+        format.html { redirect_to @building, notice: "保存に成功しました" }
         format.json { render :show, status: :created, location: @building }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -35,13 +30,12 @@ class BuildingsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /buildings/1 or /buildings/1.json
   def update
     set_building
 
     respond_to do |format|
       if @building.update(building_params)
-        format.html { redirect_to @building, notice: "Building was successfully updated." }
+        format.html { redirect_to @building, notice: "編集に成功しました" }
         format.json { render :show, status: :ok, location: @building }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -50,24 +44,21 @@ class BuildingsController < ApplicationController
     end
   end
 
-  # DELETE /buildings/1 or /buildings/1.json
   def destroy
     set_building
     @building.destroy
 
     respond_to do |format|
-      format.html { redirect_to buildings_url, notice: "Building was successfully destroyed." }
+      format.html { redirect_to buildings_url, notice: "対象を削除しました" }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_building
     @building = Building.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def building_params
     params.require(:building).permit(:build, :price, :address, :age, :remarks, stations_attributes: [:line, :station, :distance, :_destroy])
   end
